ea18cd576cb0a30ed59c88a3a4b6ac07db5867af
Justin Bueno <justinbueno63@gmail.com>
2025-12-02 05:17:10 -0800

    Added functionality to open posts. Worked on design more.

--- src/data.js
+++ src/data.js
@@ -3,8 +3,21 @@ let postInfo = [
         title:"My new journey as a bootcamp student.",
         date:"July 22, 2025",
         imagePath:"public/images/featured-post-2.jpg",
-        post:"After several months of learning in the Frontend Developer Career Path,I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
-        subheading:""
+        post: `
+            <h4>How I stay committed to learning</h4>
+
+            I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.
+
+            While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.
+
+            <h4>How I got started</h4>
+
+            I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.
+
+            That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.
+        `,
+        subheading:"After several months of learning in the Frontend Developer Career Path,I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+        uuid:"9ca3793e-e30b-4986-8f72-f1184f1cf333"
     },
     {
         title:"Blog One",
@@ -13,7 +26,10 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"7d88ca87-f99c-4baa-bac7-9041a6495cd6"
     },
     {
         title:"Blog Two",
@@ -22,7 +38,10 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"f45fcfdf-5072-4ba9-b2d3-a8b1a62bf30e"
     },
     {
         title:"Blog Three",
@@ -31,7 +50,10 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"643d6d48-da63-4d72-b61b-f28f03877035"
     },
     {
         title:"Blog Four",
@@ -40,7 +62,10 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"d7acb831-4c97-456c-b34d-61e89ab10a54"
     },
     {
         title:"Blog Five",
@@ -49,7 +74,10 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"505e961a-07f4-4294-83b2-8b6de8b5c54b"
     },
     {
         title:"Blog Six",
@@ -58,21 +86,25 @@ let postInfo = [
         post:`I'm excited to start a new learning journey 
             as a Scrimba Bootcamp student! After several months of learning
             in the Frontend Developer Career Path.`,
-        subheading:""
+        subheading:`I'm excited to start a new learning journey 
+            as a Scrimba Bootcamp student! After several months of learning
+            in the Frontend Developer Career Path.`,
+        uuid:"4d5be919-537c-4bdb-b090-2a632badfad5"
     },
-];
+].reverse();
 
 const subheadingSize = 30;  
 
 //Creating Subheading from the post in the size of subheadingSize
-postInfo = postInfo.map((post)=>{
-    return {
-            ...post,
-            subheading : post.post.replace(/<[^>]*>/g,"").split(" ").slice(0,subheadingSize).join(" ") + "..."
-    }
-})
+// postInfo = postInfo.map((post)=>{
+//     return {
+//             ...post,
+//             subheading : post.post.replace(/<[^>]*>/g,"").split(" ").slice(0,subheadingSize).join(" ") + "..."
+//     }
+// })
+
 //Establish Featured Post
-const featuredPost = postInfo[0];
+const featuredPost = postInfo[postInfo.length - 1];
 
 //Remove Featured Post from the post list
 postInfo = postInfo.filter((post)=>post!=featuredPost);
--- src/main.js
+++ src/main.js
@@ -1,11 +1,70 @@
 import {postInfo,featuredPost} from "./data";
 const postsContainer = document.querySelector("#posts-container");
-const loadMoreBtn = document.querySelector("#load-more");
+const mainEl = document.querySelector("main");
+const basePostsToRender = 3;
+const amountOfPostsToAdd = 3;
 let isHome = true;
+let isArticle = false;
+let isAboutPage = false;
 
-loadMoreBtn.addEventListener("click",()=>{
-    const currentPostsLength = postsContainer.children.length;
-    const amountOfPostsToAdd = 3;
+document.addEventListener("click", (event)=>{
+    if(event.target.dataset.postUuid){
+        handleArticle(event);
+    }
+})
+document.querySelector("#load-more").addEventListener("click",handleLoadButton);
+document.querySelector("#home").addEventListener("click", handleHomeButton);
+document.querySelector("#about-me").addEventListener("click",handleAboutMe);
+
+function handleNavigation(home,article,aboutPage){
+    if(home){
+        isHome = true;
+        isArticle = false;  
+        isAboutPage = false;   
+    } else if(article){
+        isArticle = true;
+        isHome = false;
+        isAboutPage = false;
+    } else if(aboutPage){
+        isArticle = false;
+        isHome = false;
+        isAboutPage = true;
+    }
+    renderFeaturedPost();
+    renderPosts(basePostsToRender);
+}
+
+function handleArticle(event){
+    handleNavigation(false,true,false);
+    let clickedPost = postInfo.filter((post)=> post.uuid === event.target.dataset.postUuid)[0];
+    clickedPost = clickedPost || featuredPost;
+    console.log(clickedPost)
+    mainEl.innerHTML = `
+    <p class="date">${clickedPost.date}</p>
+    <h2 class="title">${clickedPost.title}</h2>
+    <p class="subheading">${clickedPost.subheading}</p>
+    <img src="${clickedPost.imagePath}"/>
+    <div class="post-container">${clickedPost.post}</div>
+    <div class="content-seperator"></div>
+    <h3>Recent Posts</h3>
+    `;
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // smooth scrolling
+    });
+}
+
+function handleAboutMe(){
+    handleNavigation(false,false,true);
+    mainEl.innerHTML = "";
+}
+
+function handleHomeButton(){
+    handleNavigation(true,false,false);
+    mainEl.innerHTML = "";
+}
+function handleLoadButton(){
+    const currentPostsLength = postsContainer.children.length;;
     //If there's no more posts
     if(currentPostsLength === postInfo.length){
         alert("bitch stop there's no more posts");
@@ -18,37 +77,43 @@ loadMoreBtn.addEventListener("click",()=>{
     else {
         renderPosts(currentPostsLength+amountOfPostsToAdd);
     }
-})
+}
 
 function renderFeaturedPost(){
-        document.querySelector("#featured-post").innerHTML = createHTMLPost(featuredPost.title,featuredPost.date,featuredPost.imagePath,featuredPost.subheading)
+    const featuredPostEl = document.querySelector("#featured-post");
+    if(isHome){
+        featuredPostEl.innerHTML = createHTMLPost(featuredPost)
+        featuredPostEl.classList.remove("hidden");
+    } else{
+        featuredPostEl.innerHTML = "";
+        featuredPostEl.classList.add("hidden");
+    }
 }
 
 function renderPosts(amount){
-    isHome && renderFeaturedPost()    
     let output = "";
-
     if(amount>=postInfo.length){
         amount = postInfo.length
     }
-     
+
     for(let i=0; i<amount; i++){
         output+=`
         <section class="post">
-            ${createHTMLPost(postInfo[i].title,postInfo[i].date,postInfo[i].imagePath,postInfo[i].subheading)}
+            ${createHTMLPost(postInfo[i])}
         </section>
         `
     }
     postsContainer.innerHTML = output;
 }
 
-function createHTMLPost(title,date,imagePath,subheading){
+function createHTMLPost({title,date,imagePath,subheading,uuid}){
     return `
-            <img src="${imagePath}"/>
+            <img src="${imagePath}" data-post-uuid="${uuid}"/>
             <p class="date">${date}</p>
-            <h2>${title}</h2>
-            <p class="post-text">${subheading}
-            </p>`
+            <h2 data-post-uuid="${uuid}">${title}</h2>
+            <p class="subheading">${subheading}</p>
+        `
 }
 
-renderPosts(6);
+renderFeaturedPost()    
+renderPosts(basePostsToRender);
